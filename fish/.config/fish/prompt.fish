5f9e7fb4137e0b1a8a4f8e056708afa24b8a63b6
Maximilian Block <max@pool.math.tu-berlin.de>
2022-02-15 14:40:36 +0100

    fish prompt rework unfinished

--- fish/.config/fish/prompt.fish
+++ fish/.config/fish/prompt.fish
@@ -5,12 +5,44 @@ function fish_prompt --description 'Write out the prompt'
     echo (git symbolic-ref HEAD 2>/dev/null | sed -e 's|^refs/heads/||')
   end
 
-  function _is_git_dirty
-    echo (git status -s --ignore-submodules=dirty 2>/dev/null)
+  set -g git_branch (_git_branch_name)
+
+  function _current_working_directory
+    if set -q git_branch
+      set -l git_dir (git rev-parse --show-toplevel)
+      set -l this_dir (pwd)
+
+      if [ $git_dir = $this_dir ]
+        echo (string split -r -m1 / $this_dir)[2]
+      else
+        set git_dir (string split -r -m1 / $git_dir)[1]
+        string replace $git_dir "" $this_dir
+      end
+    else
+      echo (prompt_pwd)
+    end
   end
 
-  if [ (_git_branch_name) ]
-    set -l git_branch (set_color green)(_git_branch_name)
+  function _is_git_dirty
+    if set -q git_branch
+      echo (git status -s --ignore-submodules=dirty 2>/dev/null)
+    end
+  end
+
+  function _git_commit_hash
+    if set -q git_branch
+      echo (git log --pretty=format:'%h' -n 1)
+    end
+  end
+
+  function _git_commit_message
+    if set -q git_branch
+      set max_commit_length 25
+      echo (git log -1 --pretty=%B | head -n1)
+    end
+  end
+
+  if set -q git_branch
     if [ (_is_git_dirty) ]
       for i in (git branch -qv --no-color | string match -r '\*' | cut -d' ' -f4- | cut -d] -f1 | tr , \n)\
         (git status --porcelain | cut -c 1-2 | uniq)
@@ -40,16 +72,19 @@ function fish_prompt --description 'Write out the prompt'
   end
 
   set_color -b black
-  printf '%s%s@%s %s%s' (set_color -o white) (whoami) (hostname) (prompt_pwd) (set_color normal)
-  #printf ' %s%s%s' (set_color -o white) (echo $PWD | sed -e "s|^$HOME|~|") (set_color normal)
-  printf '%s' $git_info
+
+  printf '%s'        (set_color -o white)
+  printf '%s:%s '    (whoami) (_current_working_directory)
+  printf '%s '       $git_info
+  printf '%s: "%s" ' (_git_commit_hash) (_git_commit_message)
+  printf '%s'        (set_color normal)
 
   if test $laststatus -ne 0
-    printf " %s%s" (set_color -o red) "✘"
+    printf "%s%s%s" (set_color -o red) "✘" (set_color normal)
     #printf " %s%s " (set_color -o green) "✔"
   end
 
-  printf " "
+  printf "\n❯ "
 
   set_color normal
 end
